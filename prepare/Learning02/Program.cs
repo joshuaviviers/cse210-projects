a51ce0d2caefa6466dfeac8f3bdb0996edc03a98
jviviers <viv22004@byui.edu>
2024-01-16 13:48:09 -0700

    Finished project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+    if (grade >= 90)
+    {
+        Console.WriteLine("Your grade is an A. Well done!");
+    }
+    else if (grade >= 80)
+    {
+        Console.WriteLine("Your grade is a B. Well done.");
+    }
+    else if (grade >= 70)
+    {
+        Console.WriteLine("Your grade is a C.");
+    }
+    else if (grade >= 60)
+    {
+        Console.WriteLine("Your grade is a D.");
+    }
+    else
+    {
+        Console.WriteLine("Your grade is a F.");
+    }
+
+    if (grade >= 70)
+    {
+        Console.WriteLine("You Passed! Well done");
+    }
+    else
+    {
+        Console.WriteLine("Better luck next time");
+    }
     }
 }
